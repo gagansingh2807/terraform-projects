6b919e51ea025d18183b8744ae20e746c39305f3
gagansingh2807 <gaganrai5523@gmail.com>
2023-12-29 19:00:18 +0530

    created new files

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "us-east-1"
+  profile = "terraform-user"
+}
+
+# stores the terraform state file in S3
+terraform {
+  backend "s3" {
+    bucket = "gagan94-terraform-remote-state"
+    key    = "terraform.tfstate.dev"
+    region = "us-east-1"
+    profile = "terraform-user"
+  }
+}
+
